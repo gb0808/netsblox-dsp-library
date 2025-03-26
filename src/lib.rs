31a9bd32e5ec51fd59cff4623afb99fefdd6b70e
Gabe Barnard <gbarnard08@gmail.com>
2025-03-26 10:13:48 -0500

    created a function to normalize a signal

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,16 @@
 use rustfft::{FftPlanner, num_complex::Complex};
 use wasm_bindgen::prelude::*;
 
+fn normalize(signal: &mut [f32]) {
+    let mut max: f32 = 0.0;
+    signal.into_iter().for_each(|sample| {
+        if *sample > max {
+            max = *sample;
+        }
+    });
+    signal.into_iter().for_each(|sample| *sample = *sample / max);
+}
+
 #[wasm_bindgen]
 pub fn fourier_transform(signal: &[f32]) -> Vec<f32> {
     let mut planner = FftPlanner::new();

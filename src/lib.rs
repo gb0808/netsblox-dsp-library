0b15adc940f207d7aa1266ef67fd8375af6bafa4
Gabe Barnard <gbarnard08@gmail.com>
2025-04-12 18:17:43 -0500

    added ACF support

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,24 @@
 use rustfft::{FftPlanner, num_complex::Complex};
 use wasm_bindgen::prelude::*;
 
+fn autocorrelation_function_inner(signal: &[f32], lag: usize, window_size: usize) -> f32 {
+  let signal_window = &signal[..window_size];
+  let (shifted_start, shifted_end) = (lag, lag + window_size);
+  let shifted_signal_window = &signal[shifted_start..shifted_end];
+  signal_window.into_iter()
+               .zip(shifted_signal_window.into_iter())
+               .map(|(i, j)| i * j)
+               .collect::<Vec<f32>>()
+               .into_iter()
+               .sum()
+}
+
+#[wasm_bindgen]
+pub fn autocorrelation_function(signal: &[f32], window_size: usize) -> Vec<f32> {
+  let lag_iter = 0..window_size;
+  lag_iter.map(|lag| autocorrelation_function_inner(signal, lag, window_size)).collect() 
+}
+
 #[wasm_bindgen]
 pub fn normalize(signal: &[f32]) -> Vec<f32> {
   let mut max: f32 = 0.0;
